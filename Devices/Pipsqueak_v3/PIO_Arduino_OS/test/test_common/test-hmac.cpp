e01691aa7586b14e0bb8632f165b6b49d983894d
Michael Volk <michael-volk@hotmail.com>
2020-09-17 14:41:11 -0700

    Add HMAC comparison feature

--- Devices/Pipsqueak_v3/PIO_Arduino_OS/test/test_common/test-hmac.cpp
+++ Devices/Pipsqueak_v3/PIO_Arduino_OS/test/test_common/test-hmac.cpp
@@ -20,6 +20,22 @@ void test_hmac() {
   delete hmac;
 }
 
+void test_hmac_equals() {
+  const char password[] = "ThisIsATopSecret32ByteValuePad32";
+  const char message[] = "An authentic message";
+  Hmac * hmac = new Hmac((const byte *) &password, (const byte *) &message, 20);
+  byte expected_value[] = {
+    0x79, 0x00, 0x67, 0x61, 0x56, 0xB8, 0x98, 0x85,
+    0x3C, 0x85, 0xC0, 0x9F, 0xAC, 0xAC, 0xC1, 0xE1,
+    0x1E, 0xF1, 0x9E, 0x80, 0x7C, 0x5A, 0x64, 0xD8,
+    0xBA, 0x77, 0x75, 0x46, 0xD0, 0x2A, 0xE1, 0x39
+  };
+
+  TEST_ASSERT_TRUE(hmac->equals((const byte *) &expected_value));
+
+  delete hmac;
+}
+
 void setup() {
   #ifdef ARDUINO
   delay(2000);
@@ -29,5 +45,6 @@ void setup() {
 
 void loop() {
   RUN_TEST(test_hmac);
+  RUN_TEST(test_hmac_equals);
   UNITY_END();
 }
