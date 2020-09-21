b6512cebefa5a2a27618b1cba3bcacebae45cbdb
Michael Volk <michael-volk@hotmail.com>
2020-09-21 14:13:47 -0700

    Rejigger Hmac for static memory allocation

--- Devices/Pipsqueak_v3/PIO_Arduino_OS/lib/Hmac/Hmac.h
+++ Devices/Pipsqueak_v3/PIO_Arduino_OS/lib/Hmac/Hmac.h
@@ -19,6 +19,7 @@
 // Lengths expressed in bytes
 #define SHA_256_KEY_LENGTH 32
 #define SHA_256_HASH_LENGTH 32
+#define HMAC_SIZE SHA_256_HASH_LENGTH
 #define SHA_256_BLOCK_LENGTH 64
 #define SHA_256_BUFFER_SIZE 64
 
@@ -27,29 +28,33 @@
 #define SHA_256_HMAC_OPAD 0x5c
 
 /**
- * Generates an HMAC.
+ * Generates and validates HMACs.
  *
- * Not for use within ISRs.
+ * Not for use within ISRs. Not thread-safe.
  */
 class Hmac {
   public:
-    Hmac(const byte * secretKey, const byte * messageContent, size_t messageContentLength);
+    /**
+     * Construct an instance that will use the provided key.
+     */
+    Hmac(const byte * secretKey);
 
     /**
      * Writes the HMAC value to a provided buffer.
-     * The buffer's size must be at least length() bytes.
+     * The buffer's size must be at least HMAC_SIZE bytes.
      */
-    void write(void * buffer);
+    void generate(const byte * messageContent, size_t messageContentLength, byte * buffer);
 
     /**
-     * Compares the value of this HMAC to another HMAC value.
+     * Compares the value of this HMAC to another HMAC value, returning true if
+     * they are equal and false otherwise.
      */
-    bool equals(const byte * hmac);
-
-    /** Returns the length of the HMAC in bytes. */
-    size_t length();
+    bool validate(const byte * messageContent, size_t messageContentLength, const byte * hmac);
 
   private:
+    /** Computes an HMAC, storing it in the internal buffer. */
+    void compute(const byte * messageContent, size_t messageContentLength);
+
     /** Initializes the SHA256 process, setting the counter and buffers. */
     void init(void);
 
--- Devices/Pipsqueak_v3/PIO_Arduino_OS/test/test_common/test-hmac.cpp
+++ Devices/Pipsqueak_v3/PIO_Arduino_OS/test/test_common/test-hmac.cpp
@@ -2,12 +2,12 @@
 #include <unity.h>
 #include <Hmac.h>
 
-void test_hmac() {
+void test_generate() {
   const char password[] = "ThisIsATopSecret32ByteValuePad32";
   const char message[] = "An authentic message";
-  Hmac * hmac = new Hmac((const byte *) &password, (const byte *) &message, 20);
+  Hmac * hmac = new Hmac((const byte *) &password);
   byte * actual_value = new byte [32];
-  hmac->write(actual_value);
+  hmac->generate((const byte *) &message, 20, actual_value);
   byte expected_value[] = {
     0x79, 0x00, 0x67, 0x61, 0x56, 0xB8, 0x98, 0x85,
     0x3C, 0x85, 0xC0, 0x9F, 0xAC, 0xAC, 0xC1, 0xE1,
@@ -20,10 +20,10 @@ void test_hmac() {
   delete hmac;
 }
 
-void test_hmac_equals() {
+void test_validate() {
   const char password[] = "ThisIsATopSecret32ByteValuePad32";
   const char message[] = "An authentic message";
-  Hmac * hmac = new Hmac((const byte *) &password, (const byte *) &message, 20);
+  Hmac * hmac = new Hmac((const byte *) &password);
   byte expected_value[] = {
     0x79, 0x00, 0x67, 0x61, 0x56, 0xB8, 0x98, 0x85,
     0x3C, 0x85, 0xC0, 0x9F, 0xAC, 0xAC, 0xC1, 0xE1,
@@ -31,7 +31,7 @@ void test_hmac_equals() {
     0xBA, 0x77, 0x75, 0x46, 0xD0, 0x2A, 0xE1, 0x39
   };
 
-  TEST_ASSERT_TRUE(hmac->equals((const byte *) &expected_value));
+  TEST_ASSERT_TRUE(hmac->validate((const byte *) &message, 20, (const byte *) &expected_value));
 
   delete hmac;
 }
@@ -44,7 +44,7 @@ void setup() {
 }
 
 void loop() {
-  RUN_TEST(test_hmac);
-  RUN_TEST(test_hmac_equals);
+  RUN_TEST(test_generate);
+  RUN_TEST(test_validate);
   UNITY_END();
 }
